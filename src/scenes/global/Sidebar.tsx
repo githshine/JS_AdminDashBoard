237ba0267547ed3620794826875d7dc9648f002d
艾因 <aiyin@qunhemail.com>
2024-12-19 20:42:33 -0500

    [ts-migrate][src] Init tsconfig.json file
    Co-authored-by: ts-migrate <>

--- src/scenes/global/Sidebar.tsx
+++ src/scenes/global/Sidebar.tsx
@@ -37,7 +37,6 @@ const Item = ({
       onClick={() => setSelected(title)}
       icon={icon}
     >
-      // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
       <Typography>{title}</Typography>
     </MenuItem>
   );
